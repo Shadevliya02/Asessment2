9f9b334fe7cb2239816bedc81afd252499be9f6f
Sharla Devrina Aurelliya <devrinasharla@gmail.com>
2025-05-02 00:32:16 +0700

    Theme

--- app/src/main/java/com/sharla607062330139/asessment2/ui/theme/Theme.kt
+++ app/src/main/java/com/sharla607062330139/asessment2/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.sharla607062330139.asessment2.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
